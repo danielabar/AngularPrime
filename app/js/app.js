749ddd58a9748187c1cea2e0ff067dd8e07a2448
Daniela Baron <baron.daniela@gmail.com>
2013-06-14 22:29:01 -0400

    prime functions now in service

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,26 @@
 'use strict';
 
 
-// Declare app level module which depends on filters, and services
-angular.module('primeNumberApp', ['primeNumberService']);
+var PrimeNumberApp = angular.module("PrimeNumberApp", []);
+
+PrimeNumberApp.factory('PrimeNumberService', function() {
+
+	function PrimeCandidate(value, crossedOut) {
+   		this.value = value;
+   		this.crossedOut = crossedOut;
+	};
+  
+  	var PrimeNumberService = {}
+
+  	PrimeNumberService.init = function(upTo) {
+       var candidates = [];
+		for(var i=2; i<=upTo; i++) {
+			candidates[i-2] = new PrimeCandidate(i, false);
+		};
+		return candidates;
+    };
+  
+  
+  	return PrimeNumberService;
+
+});
--- app/js/primeNumberController.js
+++ app/js/primeNumberController.js
@@ -1,29 +1,7 @@
 'use strict'
 
+PrimeNumberApp.controller('PrimeNumberCtrl',
+  function($scope, PrimeNumberService) {
 
-//TODO: Inject service for primes
-function PrimeNumberCtrl($scope) {
-
-	function PrimeCandidate(value, crossedOut) {
-   		this.value = value;
-   		this.crossedOut = crossedOut;
-	};
-
-	var init = function(upTo) {
-       var candidates = [];
-		for(var i=2; i<=upTo; i++) {
-			candidates[i-2] = new PrimeCandidate(i, false);
-		};
-		return candidates;
-    };
-
- 	$scope.primeCandidates = init(100);
-
- 	$scope.findPrimes = function(upTo) {
-
- 	};
-
-};
-
-// Minify support
-PrimeNumberCtrl.$inject = ['$scope'];
+  $scope.primeCandidates = PrimeNumberService.init(100);
+});
