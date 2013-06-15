ba4feb90ce16c3e75560e4b0e650512881964bf6
Daniela Baron <baron.daniela@gmail.com>
2013-06-14 21:28:58 -0400

    service skeleton, list the prime candidates

--- app/js/primeNumberController.js
+++ app/js/primeNumberController.js
@@ -1,16 +1,27 @@
 'use strict'
 
+
 //TODO: Inject service for primes
 function PrimeNumberCtrl($scope) {
 
- 	$scope.primeCandidates = [
-    	{"value": 2,
-     	"crossedOut": false},
-    	{"value": 3,
-     	"crossedOut": false},
-    	{"value": 4,
-     	"crossedOut": false}
-  	];
+	function PrimeCandidate(value, crossedOut) {
+   		this.value = value;
+   		this.crossedOut = crossedOut;
+	};
+
+	var init = function(upTo) {
+       var candidates = [];
+		for(var i=2; i<=upTo; i++) {
+			candidates[i-2] = new PrimeCandidate(i, false);
+		};
+		return candidates;
+    };
+
+ 	$scope.primeCandidates = init(100);
+
+ 	$scope.findPrimes = function(upTo) {
+
+ 	};
 
 };
 
