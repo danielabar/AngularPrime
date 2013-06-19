ea57df66f80ccd8ed05c3e7d3801ec65ba26083b
Daniela Baron <baron.daniela@gmail.com>
2013-06-18 20:02:31 -0400

    add form so user can specify number to find primes up to

--- app/js/primeNumberController.js
+++ app/js/primeNumberController.js
@@ -1,20 +1,22 @@
 'use strict'
 
 
-//FIXME get 'upTo' from user input
 PrimeNumberApp.controller('PrimeNumberCtrl',
     function ($scope, $timeout, PrimeNumberService) {
 
-        $scope.primeCandidates = PrimeNumberService.init(100);
-
-        $timeout(function () {
-           PrimeNumberService.findPrimes(100)
-        }, 1000);
+    	//TODO: Validate upTo is numeric - is there an Angular way to do validation?
+        $scope.findPrimesAction = function() {
+        	$scope.primeCandidates = PrimeNumberService.init($scope.upTo);
+        	$timeout(function () {
+           		PrimeNumberService.findPrimes($scope.upTo)
+        	}, 500);
+        };
 
         // Events are only received if previous call to findPrimes occurs in a $timeout
         $scope.$on('foundNonPrime', function (event, primeCandidate, index) {
-            console.log('foundNonPrime event was raised for candidate: ' + primeCandidate + ' at index: ' + index);
-            $scope.primeCandidates[index].crossedOut = true;
+            $timeout(function () {
+            	$scope.primeCandidates[index].crossedOut = true;
+        	}, 2000);
         });
 
     });
