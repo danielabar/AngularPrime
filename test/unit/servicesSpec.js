7f80d47c09f0b72450ec4424d6da0319f3159c49
Daniela Baron <baron.daniela@gmail.com>
2013-06-23 16:08:36 -0400

    unit tests for prime number service

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -14,7 +14,6 @@ describe('Prime Number Service: ', function() {
   	});
 
   	describe('Initialization: ', function() {
-
   		var upTo, actual;
   		beforeEach(function() {
     		upTo = 10;
@@ -35,11 +34,38 @@ describe('Prime Number Service: ', function() {
 	  	it('Prime Candidates are NOT crossed out', function() {
 	  		for (var i = 0; i < actual.length; i++) {
 	  			expect(actual[i].crossedOut).toBe(false);
-	  		}
+	  		};
 	  	});
-
   	});
 
-  	
+  	describe('Find Primes Up To 10: ', function() {
+  		var upTo, actual;
+  		beforeEach(function() {
+    		upTo = 10;
+	  		actual = service.findPrimes(upTo);
+		});
+  		
+  		it('Total number of candidates is 10', function() {
+			expect(actual.length).toEqual(upTo-1);
+  		});
+
+  		it('There are 4 primes', function() {
+  			var primeCount = 0;
+			for (var i = 0; i < actual.length; i++) {
+				if (actual[i].crossedOut === false) {
+					primeCount = primeCount + 1;
+				};
+			};
+			expect(primeCount).toEqual(4);
+  		});
+
+  		it('Primes of 10 are: 2, 3, 5, and 7', function() {
+  			expect(actual[0].value).toEqual(2);
+  			expect(actual[1].value).toEqual(3);
+  			expect(actual[3].value).toEqual(5);
+  			expect(actual[5].value).toEqual(7);
+  		});
+
+  	});
 
 });
