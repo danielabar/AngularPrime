72467d51214686f2df369fbe09da1408703d5cc9
Daniela Baron <baron.daniela@gmail.com>
2013-06-23 19:36:14 -0400

    unit tests for controller with mock service

--- test/unit/primeNumberControllerSpec.js
+++ test/unit/primeNumberControllerSpec.js
@@ -3,11 +3,45 @@
 
 describe('Prime Number Controller', function() {
 
-	beforeEach(module('PrimeNumberApp'));
 	var scope, ctrl;
+	var mockPrimeNumberService = {
+    	init: function (upTo) {
+    		return [
+    			{value: 2, crossedOut: false},
+    			{value: 3, crossedOut: false},
+    			{value: 4, crossedOut: false},
+    			{value: 5, crossedOut: false},
+    			{value: 6, crossedOut: false},
+    			{value: 7, crossedOut: false},
+    			{value: 8, crossedOut: false},
+    			{value: 9, crossedOut: false},
+    			{value: 10, crossedOut: false}
+    		];
+    	},
+    	findPrimes: function(upTo) {
+    		return [
+    			{value: 2, crossedOut: false},
+    			{value: 3, crossedOut: false},
+    			{value: 4, crossedOut: true},
+    			{value: 5, crossedOut: false},
+    			{value: 6, crossedOut: true},
+    			{value: 7, crossedOut: false},
+    			{value: 8, crossedOut: true},
+    			{value: 9, crossedOut: true},
+    			{value: 10, crossedOut: false}
+    		];
+    	}
+  	};
+
+	beforeEach(module('PrimeNumberApp'));
+
 	beforeEach(inject(function($controller, $rootScope) {
     	scope = $rootScope.$new();
-    	ctrl = $controller('PrimeNumberCtrl', {$scope: scope});
+    	scope.upTo = 10;
+    	ctrl = $controller('PrimeNumberCtrl', {
+      		$scope: scope,
+      		PrimeNumberService: mockPrimeNumberService
+    	});
 	}));
 
   	it('Controller is defined', function() {
@@ -16,6 +50,16 @@ describe('Prime Number Controller', function() {
 
   	it('Initially there are no alerts', function() {
   		expect(scope.alerts).toEqual([]);
+  	});
+
+  	it('Initially there are no prime candidates', function() {
+  		expect(scope.primeCandidates).toEqual(null);
+  	});
+
+  	it('Find Primes Action calls service', function() {
+  		spyOn(mockPrimeNumberService, 'init');
+  		scope.findPrimesAction();
+  		expect(mockPrimeNumberService.init).toHaveBeenCalledWith(10);
   	});
 
 });
