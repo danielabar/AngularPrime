bda43a67c50dd519f7a31eaaba6cf039cad7a457
Daniela Baron <baron.daniela@gmail.com>
2013-06-23 15:32:35 -0400

    service unit test for initial crossed out

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -6,17 +6,34 @@ describe('Prime Number Service', function() {
 	beforeEach(module('PrimeNumberApp'));
 	var service;
 	beforeEach(inject(function(PrimeNumberService) {
-    	service =PrimeNumberService;
+    	service = PrimeNumberService;
 	}));
 
 	it('Service is defined', function() {
 		expect(service).toBeDefined();
   	});
 
-  	it('init creates array of prime number candidates', function() {
+  	it('Init creates array of prime number candidates, size one less than specified', function() {
   		var upTo = 10;
   		var actual = service.init(upTo);
 		expect(actual.length).toEqual(upTo-1);
+  	});
+
+  	it('Init creates array of prime number candidates, starting at 2, ending at specified value', function() {
+  		var upTo = 10;
+  		var actual = service.init(upTo);
+  		var actualStartValue = actual[0].value;
+  		var actualEndValue = actual[actual.length-1].value;
+		expect(actualStartValue).toEqual(2);
+		expect(actualEndValue).toEqual(upTo);
+  	});
+
+  	it('Initially, all prime number candidates are NOT crossed out', function() {
+  		var upTo = 10;
+  		var actual = service.init(upTo);
+  		for (var i = 0; i < actual.length; i++) {
+  			expect(actual[i].crossedOut).toBe(false);
+  		}
   	});
 
 });
