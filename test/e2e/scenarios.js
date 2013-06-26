9464441de620c10fe532821fa613721181c07e66
Daniela Baron <baron.daniela@gmail.com>
2013-06-25 20:09:58 -0400

    e2e tests for form submit and numbers display

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -13,9 +13,28 @@ describe('Prime Number App', function() {
 
   		it('index.html is the default view', function() {
     		browser().navigateTo('../../app/index.html#foo');
-    		pause();
     		expect(browser().location().url()).toBe('/');
   		});
+
+	});	
+
+	describe('Find Primes', function() {
+
+		beforeEach(function() {
+			browser().navigateTo('../../app/index.html');
+    	});
+
+    	it('No numbers are displayed before form has been submitted', function() {
+      		expect(repeater('.allNumbers div').count()).toBe(0);
+    	});
+
+		it('When form is submitted for up to 10 prime numbers, numbers 2 through 10 are displayed', function() {
+      		input('upTo').enter('10');
+      		element('#findPrimes').click();
+      		expect(repeater('.allNumbers div.primeCandidateDisplay').count()).toBe(9);
+    	});
+
+    	//TODO verify the non primes have the crossedOut css class
 
 	});	
 
